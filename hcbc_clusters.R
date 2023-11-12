9e33b816bca35992c563f0b5adf1ae7b971c6290
Lauren R Kaiser <lkaiser7@hawaii.edu>
2023-11-11 14:36:07 -1000

    Cluster analysis data for 2015 and 2019 observations with 2015 zone names added to match the 2019 methods and format

---
+++ hcbc_clusters.R
@@ -0,0 +1,101 @@
+# HCBC Data Exploration #
+# Hawaii Coral Bleaching Collaborative #
+
+### GIT VERSION CONTROL PROJECT ###
+# library(usethis)
+# use_git_config()
+# create_github_token()
+# gitcreds::gitcreds_set()
+
+### SET UP ###
+
+hcbcDir<-"C:/Users/lkais/Dropbox/PacIOOS/Projects/DAR_HCBC/"
+setwd(hcbcDir)
+
+# load packages
+# install.packages("stringr")
+library(sf)
+library(readr)
+# library(leaflet)
+# library(maps)
+# library(stringr)
+# library(tidygeocoder)
+# library(openxlsx)
+
+### DATA ###
+
+# map data
+hi_map<-read_sf(paste0(hcbcDir, "map_data/"), "Coastline")
+plot(hi_map$geometry)
+
+# include NW Hawaiian Islands 
+nwhi_map<-read_sf(paste0(hcbcDir, "map_data/"), "Coastline_NWHI")
+plot(nwhi_map$geometry)
+
+# hcbc zones for Main Hawaiian Island only
+hcbc_zones<-read_sf(paste0(hcbcDir, "HCBC_zones/"), "HCBC_Zones")
+plot(hcbc_zones)
+table(hcbc_zones$ZoneName)
+# mhi_zones<-read_sf(paste0(hcbcDir, "HCBC_zones/"), "HCBC_Zones_MHI")
+# plot(mhi_zones$geometry)
+# table(mhi_zones$ZoneName) # does not include Maui_SE
+
+list.files(paste0(hcbcDir, "2019_data/"))
+list.files(paste0(hcbcDir, "2015_data/"))
+
+# 2019 bleaching event data (Main and NW Hawaiian Islands)
+clust_2019<-read_csv(paste0(hcbcDir, "2019_data/HCBC_2019_ClusteredData.csv"))
+table(clust_2019$ZoneName)
+
+# 2015 bleaching event data (Hawaii, Maui, Lanai, Oahu only)
+clust_2015<-read_csv(paste0(hcbcDir, "2015_data/HCBC_2015_ClusterData_Observations.csv"))
+table(clust_2015$Island_Name)
+
+# check data set names
+dim(clust_2019); names(clust_2019)
+dim(clust_2015); names(clust_2015)
+
+# plot points
+plot(hi_map$geometry)
+plot(hcbc_zones[3], add = T)
+points(clust_2019$Longitude_DD_mn, clust_2019$Latitude_DD_mn, col = "white", bg = "blue", pch = 21)
+points(clust_2015$Longitude_mn, clust_2015$Latitude_mn, col = "white", bg = "red", pch = 21)
+
+head(as.data.frame(clust_2019))
+summary(clust_2019)
+head(as.data.frame(clust_2015))
+summary(clust_2015)
+
+### 2105 DATA EDITS ###
+# add counter to 2015 data
+clust_2015$obs_n<-1:dim(clust_2015)[1]
+
+# convert to spatial points data frame
+sp_2015<-st_as_sf(clust_2015, coords = c("Longitude_mn", "Latitude_mn"),
+                  crs = st_crs(hcbc_zones))
+# find intersection of polygon and points
+zones_2015<-st_intersection(hcbc_zones, sp_2015)
+# dim(zones_2015) # 33 points not within hcbc_zones
+plot(hcbc_zones$geometry[which(hcbc_zones$ISLAND == "Hawaii")])
+plot(hcbc_zones$geometry[which(hcbc_zones$ISLAND == "Maui")])
+plot(hcbc_zones$geometry[which(hcbc_zones$ISLAND == "Lanai")])
+plot(hcbc_zones$geometry[which(hcbc_zones$ISLAND == "Oahu")])
+points(clust_2015$Longitude_mn, clust_2015$Latitude_mn, col = "yellow", pch = 20)
+plot(sp_2015[1], col = 'red', pch = 20, add = T)
+plot(zones_2015$geometry, col = "blue", pch = 20, add = T)
+
+# # check mhi_zones
+# sp_2015<-st_as_sf(clust_2015, coords = c("Longitude_mn", "Latitude_mn"),
+#                   crs = st_crs(mhi_2015))
+# mhi_2015<-st_intersection(mhi_zones, sp_2015)
+# dim(mhi_zones)
+
+# add zone name to 2015 cluster data
+clust_2015$ZoneName<-NA
+clust_2015$ZoneName<-zones_2015$ZoneName[match(clust_2015$obs_n, zones_2015$obs_n)]
+table(clust_2015$ZoneName, useNA = "ifany")
+
+# save file
+write_csv(clust_2015, paste0(hcbcDir, "2015_data/HCBC_2015_ClusteredData.csv"))
+
+### END ###
